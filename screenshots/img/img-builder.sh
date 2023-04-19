0314cd6afee27ec692773429bab86fe7924e34b7
simonthi <github@simonthiefes.de>
2023-04-19 12:41:38 +0200

    Tweak for wrong filenames

--- screenshots/img/img-builder.sh
+++ screenshots/img/img-builder.sh
@@ -1,3 +1,3 @@
-for i in *.jpeg; do echo "<img src="/img/$i" width="100%">" >> "temp.html"; done
+for i in *.jpeg; do echo "<img src="./img/$i" width="100%">" >> "temp.html"; done
 cat temp.html | sort -R > images.html
 rm temp.html
